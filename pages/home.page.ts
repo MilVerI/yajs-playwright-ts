f8b429d883a0442903b9757a3b2a285bd6e76300
Veronika <vero.milevska@gmail.com>
2026-05-03 19:51:57 +0300

    test: add home, product page objects and products spec

---
+++ pages/home.page.ts
@@ -0,0 +1,28 @@
+import type { Locator, Page } from '@playwright/test';
+
+export class HomePage {
+  page: Page;
+  productsContainer: Locator;
+  productCardsList: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.productsContainer = page.locator('div[data-test].container');
+    this.productCardsList = page.locator('a[data-test^="product-"]');
+  }
+
+  async navigateToHomePage(): Promise<void> {
+    await this.page.goto('/');
+  }
+
+  getProductCard(productNameVale: string): Locator {
+    return this.productCardsList.filter({
+      has: this.page.getByText(productNameVale, { exact: true }),
+    });
+  }
+
+  async getproductPriceValue(productCard: Locator): Promise<string> {
+    const productPrice = productCard.getByTestId('product-price');
+    return ((await productPrice.textContent()) ?? '0').replace('$', '');
+  }
+}
